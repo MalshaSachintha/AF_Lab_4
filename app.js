86f8e991c46981409399b43efa08a0815ab9ceb8
Malsha Sachintha <malshasachintha0@gmail.com>
2026-05-20 23:37:42 +0530

    Added write file example

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 console.log("Hello Node.js");
 const fs = require("fs");
 
+// Read the file
 fs.readFile("file.txt", "utf8", function (err, data) {
   if (err) throw err;
   console.log(data);
+});
+const fs = require("fs");
+
+// Write to the file
+fs.writeFile("file.txt", "Hello World!", function (err) {
+  if (err) throw err;
+  console.log("File saved!");
 });

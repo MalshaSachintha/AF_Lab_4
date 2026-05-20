a867c4a86c3f66bf8b8f570ddb058355f55a9e89
Malsha Sachintha <malshasachintha0@gmail.com>
2026-05-21 00:29:30 +0530

    completed the rest of the lab-4 from http request

--- app.js
+++ app.js
@@ -24,3 +24,59 @@ http
   .listen(8080);
 
 console.log("Server running on at http://localhost:8080");
+
+// http request
+const https = require("https");
+
+https
+  .get("https://jsonplaceholder.typicode.com/posts/1", (resp) => {
+    let data = "";
+
+    resp.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    resp.on("end", () => {
+      console.log(JSON.parse(data));
+    });
+  })
+  .on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+
+// my module
+const myModule = require("./my-Module.js");
+console.log(myModule.myFunction());
+
+// promises
+const condition = true; // Change this to false to test rejection
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (condition) {
+      resolve("success!");
+    } else {
+      reject("Failure!");
+    }
+  }, 3000);
+});
+
+myPromise
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+// async/await
+
+async function myFunction() {
+  try {
+    const result = await myPromise;
+    console.log(result);
+  } catch (error) {
+    console.log("Error: " + error);
+  }
+}
+
+myFunction();
